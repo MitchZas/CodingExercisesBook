7a22f836dd74762178250edc85c8f1074909ed26
MitchZas <mitchellzasa@gmail.com>
2025-09-12 08:05:57 -0400

    Tried doing things

--- Level3HelloWorld/ConsolasAndTelim.cs
+++ Level3HelloWorld/ConsolasAndTelim.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Bread is ready.");
-Console.WriteLine("Whos is the bread for?");
-string name = Console.ReadLine();
-Console.WriteLine("Noted: " + name + " got bread.");
+﻿//Console.WriteLine("Bread is ready.");
+//Console.WriteLine("Whos is the bread for?");
+//string name = Console.ReadLine();
+//Console.WriteLine("Noted: " + name + " got bread.");
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+string c = "of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
